a77773542e8b02702b908ffd9179f6f03514b5d9
ndifah04 <105841116322@student.unismuh.ac.id>
2025-06-11 18:51:15 +0800

    CORS

--- src/util/server.js
+++ src/util/server.js
@@ -2,7 +2,12 @@ import { server as _server } from '@hapi/hapi';
 
 const server = _server({
     port: 3000,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+        cors: {
+            origin: ['*']
+        }
+    }
 });
 
 export default server;
